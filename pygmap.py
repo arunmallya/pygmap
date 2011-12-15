e359ebd396c4382bf7101913712a2812deaa0e3c
Arun Mallya <arun.mallya@gmail.com>
2011-12-15 17:12:17 +0530

    Colors and extra info. added

--- pygmap.py
+++ pygmap.py
@@ -32,7 +32,7 @@ def print_body(fout): # prints the required <body>
 # print_body() ends
 
 
-def print_init(fout, input_file):
+def print_init(fout, input_file, options):
     # find the center of points in the input file
     fin = open(input_file, "r")
 
@@ -71,6 +71,15 @@ def print_init(fout, input_file):
     };
 
     var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+    var image0 = './images/gmap_00FFFF_icon.png';
+    var image1 = './images/gmap_99FF00_icon.png';
+    var image2 = './images/gmap_blue_icon.png';
+    var image3 = './images/gmap_CC9900_icon.png';
+    var image4 = './images/gmap_FF00FF_icon.png';
+    var image5 = './images/gmap_FF0000_icon.png';
+    var image6 = './images/gmap_FF9900_icon.png';
+    var image7 = './images/gmap_FFFF00_icon.png';
+
 """ %(c_latitude, c_longitude))
 
 # write out markers for each of the points and the info containing its location
@@ -79,6 +88,24 @@ def print_init(fout, input_file):
     counter = 0
     for line in fin:
         info = line.split(" ")
+
+        # here are the default values for many of the possible input parameters
+        string = "" # no additional information is default
+        color = 5 # red is default
+
+        # check which of the input parameters are used
+        if options[0] == True: # use specified color
+            color = int(info[2])
+        if options[1] == True: # has additional information
+            # if color was used, then extra info will be from info[3], else info[2]
+            if options[0] == True:
+                string = str(info[3:])
+            else:
+                string = str(info[2:])
+            string = string.replace('[', '')
+            string = string.replace(']', '')
+            string = string.replace(',', ' ')
+            string = string.replace('\'', '')
 
         latitude  = float(info[0])
         longitude = float(info[1])
@@ -90,6 +117,7 @@ def print_init(fout, input_file):
     '<div id="bodyContent">'+ 
     '<p> Latitude : %s </p> ' + 
     '<p> Longitude : %s </p> ' + 
+    '<p> %s </p>'
     '</div>'+ 
     '</div>';
  
@@ -102,13 +130,14 @@ def print_init(fout, input_file):
     var marker%d = new google.maps.Marker({
            position: myLatLang%d,
            map: map,
-           title: 'Point_%d' 
+           title: 'Point_%d', 
+           icon: image%d
     });
 
     google.maps.event.addListener(marker%d, 'click', function(){
            infoWindow%d.open(map, marker%d);
     });
-""" %(counter, latitude, longitude, counter, counter, counter, latitude, longitude, counter, counter, counter, counter, counter, counter))
+""" %(counter, latitude, longitude, string, counter, counter, counter, latitude, longitude, counter, counter, counter, color, counter, counter, counter))
         counter += 1
 
 
@@ -122,11 +151,13 @@ def print_init(fout, input_file):
 # print_init() ends
 
 
-def create_map(input_file, output_file):
+def create_map(input_file, output_file, options=[]):
+    print len(options)
+
     fout = open(output_file, "w")
     
     print_header(fout)
-    print_init(fout, input_file)
+    print_init(fout, input_file, options)
     print_body(fout)
 
 # create_map() ends
